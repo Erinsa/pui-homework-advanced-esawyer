77f515ac64dc4ab723f13634a890211004476229
Erinsa <143417090+Erinsa@users.noreply.github.com>
2023-09-23 19:00:14 -0400

    HW 3 Organization

--- solution-hw3/src/views/home/header.js
+++ solution-hw3/src/views/home/header.js
@@ -1,27 +1,28 @@
+// Used lab activity (notecard) for reference
+
 import React, { Component } from 'react';
-import './index.css';
+import './header.css';
 
 class HeaderChunk extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-      
-//     };    
-//   }
+  constructor(props) {
+    super(props);
+    this.state = {};    
+  }
 
   render() {
     return (
-        <div class = "header">
+      
+      <div className = "header">
 
-      <div class = "logo">
-        <img src= "assets/logo/logo-01.svg" width = "400" alt = "Bun Bun bakeshop logo" />
-      </div>
+        <div class = "logo">
+          <img src= "assets/logo/logo-01.svg" width = "400" alt = "Bun Bun bakeshop logo" />
+        </div>
 
-      <div class = "header-right">
+        <div class = "header-right">
 
           <div class = "header-nav">
             <div class = "product">
-            <a href="#products_page" class = "product_link">PRODUCTS</a>
+              <a href="#products_page" class = "product_link">PRODUCTS</a>
             </div>
             <div class = "cart">
               CART
@@ -29,8 +30,6 @@ class HeaderChunk extends Component {
           </div>
 
           <hr class="horizontal_line" />
-          {/* Used this link for help with line styling 
-          https://www.w3schools.com/howto/howto_css_style_hr.asp/ */}
 
           <div class = "slogan_box">
             <div class = "slogan">
---
+++ solution-hw3/src/views/home/index.js
@@ -0,0 +1,118 @@
+// Used lab activity (notecard) for reference
+
+import React, { Component } from 'react';
+import './index.css';
+import RollCard from './rollcard.js';
+import HeaderChunk from './header.js';
+
+class HomePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      rollcardData: [
+        {
+          roll_img_src: "assets/products/original-cinnamon-roll.jpg",
+          roll_figcaption: "Original cinnamon roll",
+          roll_price: "2.49",
+          roll_img_alt: "Picture of original cinnamon roll"
+        },
+        {
+          roll_img_src: "assets/products/apple-cinnamon-roll.jpg",
+          roll_figcaption: "Apple cinnamon roll",
+          roll_price: "3.49",
+          roll_img_alt: "Picture of apple cinnamon roll"
+        },
+        {
+          roll_img_src: "assets/products/raisin-cinnamon-roll.jpg",
+          roll_figcaption: "Raisin cinnamon roll",
+          roll_price: "2.99",
+          roll_img_alt: "Picture of raisin cinnamon roll"
+        },
+        {
+          roll_img_src: "assets/products/walnut-cinnamon-roll.jpg",
+          roll_figcaption: "Walnut cinnamon roll",
+          roll_price: "3.49",
+          roll_img_alt: "Picture of walnut cinnamon roll"
+        },
+        {
+          roll_img_src: "assets/products/double-chocolate-cinnamon-roll.jpg",
+          roll_figcaption: "Double-chocolate cinnamon roll",
+          roll_price: "3.99",
+          roll_img_alt: "Picture of double chocolate cinnamon roll"
+        },
+        {
+          roll_img_src: "assets/products/strawberry-cinnamon-roll.jpg",
+          roll_figcaption: "Strawberry cinnamon roll",
+          roll_price: "3.99",
+          roll_img_alt: "Picture of strawberry cinnamon roll"
+        }
+      ],
+    }
+  }
+  
+  render() {
+    return (
+
+      <div className = "HomePage">
+
+        <div id ="container">
+
+          <div class = "header_stuff">
+            <HeaderChunk/>
+          </div>
+
+          <div class = "section" id = "products_page">
+            <div id="rollcard-list"> 
+              <RollCard
+                cardIndex = {0}
+                roll_img_src = {this.state.rollcardData[0].roll_img_src}
+                roll_img_alt = {this.state.rollcardData[0].roll_img_alt}
+                roll_figcaption = {this.state.rollcardData[0].roll_figcaption}
+                roll_price = {this.state.rollcardData[0].roll_price}/>
+              <RollCard
+                cardIndex = {1}
+                roll_img_src = {this.state.rollcardData[1].roll_img_src}
+                roll_img_alt = {this.state.rollcardData[1].roll_img_alt}
+                roll_figcaption = {this.state.rollcardData[1].roll_figcaption}
+                roll_price = {this.state.rollcardData[1].roll_price}/>
+              <RollCard
+                cardIndex = {2}
+                roll_img_src = {this.state.rollcardData[2].roll_img_src}
+                roll_img_alt = {this.state.rollcardData[2].roll_img_alt}
+                roll_figcaption = {this.state.rollcardData[2].roll_figcaption}
+                roll_price = {this.state.rollcardData[2].roll_price}/>
+            </div>
+
+            <div id="rollcard-list"> 
+              <RollCard
+                  cardIndex = {3}
+                  roll_img_src = {this.state.rollcardData[3].roll_img_src}
+                  roll_img_alt = {this.state.rollcardData[3].roll_img_alt}
+                  roll_figcaption = {this.state.rollcardData[3].roll_figcaption}
+                  roll_price = {this.state.rollcardData[3].roll_price}/>
+                <RollCard
+                  cardIndex = {4}
+                  roll_img_src = {this.state.rollcardData[4].roll_img_src}
+                  roll_img_alt = {this.state.rollcardData[4].roll_img_alt}
+                  roll_figcaption = {this.state.rollcardData[4].roll_figcaption}
+                  roll_price = {this.state.rollcardData[4].roll_price}/>
+                <RollCard
+                  cardIndex = {5}
+                  roll_img_src = {this.state.rollcardData[5].roll_img_src}
+                  roll_img_alt = {this.state.rollcardData[5].roll_img_alt}
+                  roll_figcaption = {this.state.rollcardData[5].roll_figcaption}
+                  roll_price = {this.state.rollcardData[5].roll_price}/>
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    );
+  }
+}
+
+export default HomePage;
+
