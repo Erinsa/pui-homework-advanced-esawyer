4b0451b2ce4bef8915ab776ba4d23c0adbbe7a08
Erinsa <143417090+Erinsa@users.noreply.github.com>
2023-12-02 23:07:14 -0500

    Core mechanics 1

--- finalproject-draft1/src/views/home/card.js
+++ finalproject-draft1/src/views/home/card.js
@@ -7,40 +7,172 @@ import './card.css';
 class Card extends Component {
   constructor(props) {
     super(props);
+    this.toggleFlip.bind(this); 
+    this.toggleFlipAction.bind(this); 
     this.state = {
       flip: false,
-      hasFlippedCard: false,
-      firstCard: "",
-      secondCard: "",
+      matched: false,
+      // hasFlippedCard: false,
+      // firstCard: "",
+      // secondCard: "",
     };   
   }
 
 
-  toggleFlip() {
+  // toggleFlip() {
 
-    let hasFlippedCard_var = this.state.hasFlippedCard;
-    let firstCard_var = this.state.firstCard;
+  //   let hasFlippedCard_var = this.state.hasFlippedCard;
+  //   let firstCard_var = this.state.firstCard;
 
-    if (this.state.hasFlippedCard == true) {
-      if (this.props.card_match == this.state.firstCard.card_match){
-        this.state.firstCard.removeEventListener('click');
-        this.removeEventListener('click');
+  //   if (this.state.hasFlippedCard == true) {
+  //     if (this.props.card_match == this.state.firstCard.card_match){
+  //       this.state.firstCard.removeEventListener('click');
+  //       this.removeEventListener('click');
+  //     }
+  //   }
+
+  //   if (this.state.hasFlippedCard == false) {
+  //     console.log("flippy",)
+  //     let hasFlippedCard_var = true;
+  //     let firstCard_var = this;
+  //   }
+
+
+  //   this.setState({
+  //     flip: !this.state.flip,
+  //     // hasFlippedCard: hasFlippedCard_var,
+  //     // firstCard: firstCard_var
+  //   })
+  //   console.log(this.state.flip)
+
+  //   this.props.setParentFlippedCard(hasFlippedCard_var)
+
+  //   // if (!this.state.hasFlippedCard) {
+  //   //   hasFlippedCard = true;
+  //   //   firstCard = this;
+  //   // }
+  // };
+
+  // check_match
+
+  // toggleFlip() {
+  //   let new_flip_state = !this.state.flip;
+  //   this.setState({
+  //     flip: new_flip_state,
+  //     // hasFlippedCard: hasFlippedCard_var,
+  //     // firstCard: firstCard_var
+  //   })
+  // }
+
+  toggleFlipAction() {
+
+    console.log("flip_state 1", this.state.flip)
+
+    // this.setState({
+    //   flip: !this.state.flip,
+    //   // hasFlippedCard: hasFlippedCard_var,
+    //   // firstCard: firstCard_var
+    // })
+    // console.log("flip_state 2", this.state.flip)
+
+    // this.setState({
+    //   flip: true,
+    //   // hasFlippedCard: hasFlippedCard_var,
+    //   // firstCard: firstCard_var
+    // })
+    // console.log("wassup", this.props.hasFlippedCard);
+    let hasFlippedCard_var = this.props.hasFlippedCard;
+    let firstCard_var = this.props.firstCard;
+    let secondCard_var = this.props.secondCard;
+
+    if (hasFlippedCard_var == true) {
+      this.props.setParentSecondCard(this)
+      // console.log("match 1:", this.props.card_match, "match 2:", this.props.firstCard.props.card_match);
+      if (this.props.card_match == this.props.firstCard.props.card_match){
+        console.log("match");
+        // this.props.firstCard.removeEventListener('click');
+        // this.removeEventListener('click');
+        // console.log("hello");
+        setTimeout(() => { 
+          let new_match_state = true;
+          this.props.setParentMatch(new_match_state)
+        }, 1000); 
+
+        setTimeout(() => { 
+          let matched_first_card = this.props.firstCard;
+          matched_first_card.state.matched = true;
+          // this.props.firstCard.state.flip = false;
+          let new_matched_state = true;
+          let new_match_state2 = false;
+          this.setState({
+            matched: new_matched_state,
+            // hasFlippedCard: hasFlippedCard_var,
+            // firstCard: firstCard_var
+          })
+          this.props.setParentFirstCard(matched_first_card)
+          this.props.setParentMatch(new_match_state2)
+
+      }, 2000); 
+        
+
       }
+      else {
+        console.log("no match");
+        // this.props.firstCard.flip = false;
+        // this.setState({
+        //   flip: false,
+        //   // hasFlippedCard: hasFlippedCard_var,
+        //   // firstCard: firstCard_var
+        // })
+        setTimeout(() => { 
+          let flipped_first_card = this.props.firstCard;
+          flipped_first_card.state.flip = false;
+          // this.props.firstCard.state.flip = false;
+          let new_flip_state = !this.state.flip;
+          this.setState({
+            flip: new_flip_state,
+            // hasFlippedCard: hasFlippedCard_var,
+            // firstCard: firstCard_var
+          })
+          this.props.setParentFirstCard(flipped_first_card)
+
+      }, 2000); 
+        // this.setState({
+        //   flip: new_flip_state,
+        //   // hasFlippedCard: hasFlippedCard_var,
+        //   // firstCard: firstCard_var
+        // })
+        // // this.props.setParentmatch(false)
+
+      }
+      // this.props.setParentFirstCard("")
+      // this.props.setParentSecondCard("")
+      this.props.setParentFlippedCard(false)
     }
 
-    if (this.state.hasFlippedCard == false) {
+    if (this.props.hasFlippedCard == false) {
       console.log("flippy",)
-      let hasFlippedCard_var = true;
-      let firstCard_var = this;
+      hasFlippedCard_var = true;
+      firstCard_var = this;
+      console.log(firstCard_var)
+      this.props.setParentFirstCard(firstCard_var)
+      this.props.setParentFlippedCard(hasFlippedCard_var)
+
     }
 
+    // this.setState({
+    //   flip: true,
+    //   // hasFlippedCard: hasFlippedCard_var,
+    //   // firstCard: firstCard_var
+    // })
 
-    this.setState({
-      flip: !this.state.flip,
-      hasFlippedCard: hasFlippedCard_var,
-      firstCard: firstCard_var
-    })
-    console.log(this.state.flip)
+
+    // this.setState({
+    //   flip: !this.state.flip,
+    //   // hasFlippedCard: hasFlippedCard_var,
+    //   // firstCard: firstCard_var
+    // })
+    // console.log(this.state.flip)
 
     // if (!this.state.hasFlippedCard) {
     //   hasFlippedCard = true;
@@ -48,14 +180,47 @@ class Card extends Component {
     // }
   };
 
+  toggleFlip() {
+    let new_flip_state = !this.state.flip;
+    this.setState({
+      flip: new_flip_state,
+      // hasFlippedCard: hasFlippedCard_var,
+      // firstCard: firstCard_var
+    }, this.toggleFlipAction)
+  }
+
   render() {
 
     const flip_state = {
       transform: this.state.flip ? 'rotatey(180deg)' : '', 
       // transform: this.state.flip == true ? `rotateY(-180deg)` : `rotateY(-180deg)`,
+      transition: "transform 1s",
+      transformStyle: "preserve-3d"
+    };
+
+    const visible_state = {
+      visibility: this.state.matched ? 'hidden' : 'visible', 
+      opacity: this.state.matched ? 0 : 1,
+      transition: "visibility 0.3s linear,opacity 0.3s linear"
+      // transform: this.state.flip == true ? `rotateY(-180deg)` : `rotateY(-180deg)`,
+      // transition: "transform 2s",
+      // transformStyle: "preserve-3d"
+    };
+
+     const flip_matched_state = {
+      visibility: this.state.matched ? 'hidden' : 'visible',
+      transform: this.state.flip ? 'rotatey(180deg)' : '', 
+      // transform: this.state.flip == true ? `rotateY(-180deg)` : `rotateY(-180deg)`,
       transition: "transform 2s",
       transformStyle: "preserve-3d"
     };
+
+    // const visible_state = {
+    //   visibility: this.state.matched ? 'hidden' : 'visible', 
+    //   // transform: this.state.flip == true ? `rotateY(-180deg)` : `rotateY(-180deg)`,
+    //   // transition: "transform 2s",
+    //   // transformStyle: "preserve-3d"
+    // };
 
     return (
 
@@ -65,9 +230,10 @@ class Card extends Component {
           // <div className='holder' onClick={() => {this.toggleFlip()}} style = {flip_state}>
 
             // {/* <div classname = "outer">  */}
-                <div className= "memory_card" onClick={() => {this.toggleFlip()}} style = {flip_state}>
-                    <img className = "back-face" src = {this.props.card_back_img_src} width = "200" alt = {this.props.card_back_img_alt} />
-                    <img className = "front-face" src = {this.props.card_front_img_src} width = "200" alt = {this.props.card_front_img_alt} />
+                // <div className= "memory_card" onClick={() => {{this.toggleFlip()}; {this.toggleFlipAction()}}} style = {flip_state}>
+                <div className= "memory_card" onClick={() => {{this.toggleFlip()}}} style = {flip_matched_state}>
+                    <img className = "back-face" src = {this.props.card_back_img_src} width = "200" alt = {this.props.card_back_img_alt} style={visible_state} />
+                    <img className = "front-face" src = {this.props.card_front_img_src} width = "200" alt = {this.props.card_front_img_alt} style={visible_state}  />
                 </div>
             // {/* </div> */}
 
--- finalproject-draft1/src/views/home/index.js
+++ finalproject-draft1/src/views/home/index.js
@@ -1,6 +1,7 @@
 // Used lab activity (notecard) for reference
 
 import React, { Component } from 'react';
+import { useState } from 'react';
 import './index.css';
 import Card from './card.js';
 // import Header from './header.js';
@@ -83,83 +84,108 @@ class HomePage extends Component {
           front_img_src: "assets/cards/Day_Card_Japanese.png",
           front_img_alt: "Picture",
         },
-        {
-          card_name: "Tree_Eng",
-          term_text: "Tree_Eng",
-          match: "Tree",
-          back_img_src: "assets/cards/Card_Back.png",
-          back_img_alt: "Picture",
-          front_img_src: "assets/cards/Tree_Card_English.png",
-          front_img_alt: "Picture",
-        },
-        {
-          card_name: "Person_Jpn",
-          term_text: "Person_Jpn",
-          match: "Person",
-          back_img_src: "assets/cards/Card_Back.png",
-          back_img_alt: "Picture",
-          front_img_src: "assets/cards/Person_Card_Japanese.png",
-          front_img_alt: "Picture",
-        },
-        {
-          card_name: "Person_Eng",
-          term_text: "Person_Eng",
-          match: "Person",
-          back_img_src: "assets/cards/Card_Back.png",
-          back_img_alt: "Picture",
-          front_img_src: "assets/cards/Person_Card_English.png",
-          front_img_alt: "Picture",
-        },
-        {
-          card_name: "Tree_Jpn",
-          term_text: "Tree_Jpn",
-          match: "Tree",
-          back_img_src: "assets/cards/Card_Back.png",
-          back_img_alt: "Picture",
-          front_img_src: "assets/cards/Tree_Card_Japanese.png",
-          front_img_alt: "Picture",
-        },
-        {
-          card_name: "Month_Eng",
-          term_text: "Month_Eng",
-          match: "Month",
-          back_img_src: "assets/cards/Card_Back.png",
-          back_img_alt: "Picture",
-          front_img_src: "assets/cards/Month_Card_English.png",
-          front_img_alt: "Picture",
-        },
-        {
-          card_name: "Month_Jpn",
-          term_text: "Month_Jpn",
-          match: "Month",
-          back_img_src: "assets/cards/Card_Back.png",
-          back_img_alt: "Picture",
-          front_img_src: "assets/cards/Month_Card_Japanese.png",
-          front_img_alt: "Picture",
-        },
-        {
-          card_name: "Day_Eng",
-          term_text: "Day_Eng",
-          match: "Day",
-          back_img_src: "assets/cards/Card_Back.png",
-          back_img_alt: "Picture",
-          front_img_src: "assets/cards/Day_Card_English.png",
-          front_img_alt: "Picture",
-        },
-        {
-          card_name: "Day_Jpn",
-          term_text: "Day_Jpn",
-          match: "Day",
-          back_img_src: "assets/cards/Card_Back.png",
-          back_img_alt: "Picture",
-          front_img_src: "assets/cards/Day_Card_Japanese.png",
-          front_img_alt: "Picture",
-        },
-      ]
+        // {
+        //   card_name: "Tree_Eng2",
+        //   term_text: "Tree_Eng",
+        //   match: "Tree",
+        //   back_img_src: "assets/cards/Card_Back.png",
+        //   back_img_alt: "Picture",
+        //   front_img_src: "assets/cards/Tree_Card_English.png",
+        //   front_img_alt: "Picture",
+        // },
+        // {
+        //   card_name: "Person_Jpn2",
+        //   term_text: "Person_Jpn",
+        //   match: "Person",
+        //   back_img_src: "assets/cards/Card_Back.png",
+        //   back_img_alt: "Picture",
+        //   front_img_src: "assets/cards/Person_Card_Japanese.png",
+        //   front_img_alt: "Picture",
+        // },
+        // {
+        //   card_name: "Person_Eng2",
+        //   term_text: "Person_Eng",
+        //   match: "Person",
+        //   back_img_src: "assets/cards/Card_Back.png",
+        //   back_img_alt: "Picture",
+        //   front_img_src: "assets/cards/Person_Card_English.png",
+        //   front_img_alt: "Picture",
+        // },
+        // {
+        //   card_name: "Tree_Jpn2",
+        //   term_text: "Tree_Jpn",
+        //   match: "Tree",
+        //   back_img_src: "assets/cards/Card_Back.png",
+        //   back_img_alt: "Picture",
+        //   front_img_src: "assets/cards/Tree_Card_Japanese.png",
+        //   front_img_alt: "Picture",
+        // },
+        // {
+        //   card_name: "Month_Eng2",
+        //   term_text: "Month_Eng",
+        //   match: "Month",
+        //   back_img_src: "assets/cards/Card_Back.png",
+        //   back_img_alt: "Picture",
+        //   front_img_src: "assets/cards/Month_Card_English.png",
+        //   front_img_alt: "Picture",
+        // },
+        // {
+        //   card_name: "Month_Jpn2",
+        //   term_text: "Month_Jpn",
+        //   match: "Month",
+        //   back_img_src: "assets/cards/Card_Back.png",
+        //   back_img_alt: "Picture",
+        //   front_img_src: "assets/cards/Month_Card_Japanese.png",
+        //   front_img_alt: "Picture",
+        // },
+        // {
+        //   card_name: "Day_Eng2",
+        //   term_text: "Day_Eng",
+        //   match: "Day",
+        //   back_img_src: "assets/cards/Card_Back.png",
+        //   back_img_alt: "Picture",
+        //   front_img_src: "assets/cards/Day_Card_English.png",
+        //   front_img_alt: "Picture",
+        // },
+        // {
+        //   card_name: "Day_Jpn2",
+        //   term_text: "Day_Jpn",
+        //   match: "Day",
+        //   back_img_src: "assets/cards/Card_Back.png",
+        //   back_img_alt: "Picture",
+        //   front_img_src: "assets/cards/Day_Card_Japanese.png",
+        //   front_img_alt: "Picture",
+        // },
+      ],
+      score: 0,
+      hasFlippedCard: false,
+      firstCard: "",
+      secondCard: "",
+      match: false,
 
     };
 
   };
+
+  checkState = () => { 
+    console.log("parent state", this.state.firstCard); 
+  } 
+
+  setParentFlippedCard = (new_flip_state) => { 
+    this.setState({hasFlippedCard: new_flip_state}); 
+  } 
+
+  setParentFirstCard = (new_first_card) => { 
+    this.setState({firstCard: new_first_card}); 
+  } 
+
+  setParentSecondCard = (new_second_card) => { 
+    this.setState({secondCard: new_second_card}); 
+  } 
+
+  setParentMatch = (new_match) => { 
+    this.setState({match: new_match}); 
+  }
 
   // toggleFlip() {
   //   this.setState({
@@ -167,17 +193,70 @@ class HomePage extends Component {
   //   })
   // };
 
+  // toggleFlip() {
+
+  //   let hasFlippedCard_var = this.state.hasFlippedCard;
+  //   let firstCard_var = this.state.firstCard;
+
+  //   if (this.state.hasFlippedCard == true) {
+  //     if (this.props.card_match == this.state.firstCard.card_match){
+  //       this.state.firstCard.removeEventListener('click');
+  //       this.removeEventListener('click');
+  //     }
+  //   }
+
+  //   if (this.state.hasFlippedCard == false) {
+  //     console.log("flippy",)
+  //     let hasFlippedCard_var = true;
+  //     let firstCard_var = this;
+  //   }
+
+
+  //   this.setState({
+  //     flip: !this.state.flip,
+  //     hasFlippedCard: hasFlippedCard_var,
+  //     firstCard: firstCard_var
+  //   })
+  //   console.log(this.state.flip)
+
+  //   // if (!this.state.hasFlippedCard) {
+  //   //   hasFlippedCard = true;
+  //   //   firstCard = this;
+  //   // }
+  // };
+
   render() {
+
+    const message_state = {
+      visibility: this.state.match ? 'visible': 'hidden', 
+      opacity: this.state.matched ? 0 : 1,
+      transition: "visibility 0.3s linear,opacity 0.3s linear"
+      // transform: this.state.flip == true ? `rotateY(-180deg)` : `rotateY(-180deg)`,
+      // transition: "transform 2s",
+      // transformStyle: "preserve-3d"
+    };
 
     return (
     
 
       <div className = "Outside">
       <div className = "HomePage">
-      <button className='help_button'>How to Play</button>
-      <button className='menu_button'>Moji Match</button>
+        <button className='help_button' onClick={() => {this.checkState()}}>How to Play</button>
+        <button className='menu_button'>Moji Match</button>
+        <div className='matches_counter'>
+          Matches:
+          <div className = "score_holder">
+            {this.state.score}
+          </div>
+          <button className='expand_button'></button>
+        </div>
+
+        <div className='match_message' style={message_state}>
+          MATCH!
+        </div>
 
             <div className = "rollcard-list"> 
+
 
               {this.state.cardData.map((matchcard, idx) => 
                    <Card
@@ -189,10 +268,16 @@ class HomePage extends Component {
                     card_front_img_src={matchcard.front_img_src}
                     card_front_img_alt={matchcard.front_img_alt}
                     card_match = {matchcard.match}
-                    // card_has_flipped = {hasFlippedCard}
+                    hasFlippedCard = {this.state.hasFlippedCard}
                     // firstCard = {}
+                    firstCard = {this.state.firstCard}
+                    secondCard = {this.state.secondCard}
 
-                    onToggleFlip = {this.toggleFlip}
+                    // onToggleFlip = {this.toggleFlip}
+                    setParentFlippedCard = {this.setParentFlippedCard}
+                    setParentFirstCard = {this.setParentFirstCard}
+                    setParentSecondCard = {this.setParentSecondCard}
+                    setParentMatch = {this.setParentMatch}
                     />
               )}
 
