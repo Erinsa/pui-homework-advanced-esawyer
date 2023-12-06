0b2a4766f734003516565779857dfde2a8ab6582
Erinsa <143417090+Erinsa@users.noreply.github.com>
2023-12-06 00:36:50 -0500

    routes

--- finalproject-draft1/src/index.js
+++ finalproject-draft1/src/index.js
@@ -8,9 +8,9 @@ import { HashRouter, BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
